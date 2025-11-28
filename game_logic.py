0e4ab25f4e5ac95e74a7b18f174002acea135b62
JyotiradityaParida <jyotiradityaparida12@gmail.com>
2025-11-28 15:06:34 +0530

    fixed minor bugs

--- game_logic.py
+++ game_logic.py
@@ -7,10 +7,10 @@ implements game logic
 - rotating the board for directional movement
 - performing left/up/right/down moves
 - checking for game-over state
-
 """
 
 import random
+
 #fn used to add random tile to the board. it adds 2 with 90% chance else adds 4 to an empty location(contains 0)
 def add_random_tile(board):
     pos= []
@@ -32,39 +32,42 @@ def add_random_tile(board):
 def slide(row):
     new = []
     for v in row:
-        if v != 0:
+        if (v!=0):
             new.append(v)            #create new list with non empty tiles
     for i in range(4 - len(new)):
         new.append(0)                #add remaining zeroes to the right of non empty tiles
     return new
 
-
 #it merges two tiles of same value by doubling the one on left and making the other one empty atmost once each move
 # eg. 2 2 4 0 => 4 0 4 0
 def merge(row):
+    gain = 0
     for i in range(3):
         if row[i]==row[i+1] and row[i]!=0:            #if two tiles side-by-side have same non zero value, they are merged
-            row[i]*=2
-            row[i+1]=0
-    return row
+            row[i] *= 2
+            gain += row[i]        # add only merged tile value to score
+            row[i+1] = 0
+    return row, gain
 
 #it moves the board left by first sliding all rows, merging tiles and again sliding them 
 # eg. 2 2 0 4 => 2 2 4 0 => 4 0 4 0 => 4 4 0 0
 def move_left(board):
     result = []
+    total_gain = 0
     for row in board:
         row = slide(row)
-        row = merge(row)
+        row, g = merge(row)      # collect gained score
+        total_gain += g
         row = slide(row)
         result.append(row)
-    return result
+    return result, total_gain
 
 #it rotates board 90 degrees clockwise, used for the next function
 def rotate(board):
     new = [[0]*4 for i in range(4)]
     for r in range(4):
         for c in range(4):
-            new[c][3-r] = board[r][c]        #(r,c) -> (c,3-r) => rotating board 90 degrees clockwise
+            new[c][3-r] = board[r][c]
     return new
 
 #as the code only moves board to the left we can use rotate to reuse the code for the other directions because it is easier than writing each separately
@@ -72,21 +75,20 @@ def rotate(board):
 def move_board(board, key):
     keys = {"a":0,"s":1,"d":2,"w":3}
     d = keys[key]
-    cpy = [r[:] for r in board]              #create a copy of the board
+    
+    new = [r[:] for r in board]              #create a copy of the board
     for i in range(d):
-        cpy = rotate(cpy)
-    new = move_left(cpy)
-    if d == 0: ran = 0                       #if we have to move left, no need of rotating board back to original position
+        new = rotate(new)
+
+    new,gain = move_left(new)    # get score from merge only
+
+    if d==0: ran = 0                       #if we have to move left, no need of rotating board back to original position
     else: ran = 4-d
     for j in range(ran):
         new = rotate(new)                    #board is rotated back to original position
-    moved = (new != board) #if new is different from board it means board has actually moved
-    #that is useful because we are not supposed to add new tile if the board hasn't moved 
-    gain = 0        #score added after this move
-    for r in range(4):
-        for c in range(4):
-            if new[r][c] > board[r][c]:
-                gain += new[r][c] - board[r][c]        #if new tile value is greater than old tile value, points are increased
+
+    moved = (new!=board) #if new is different from board it means board has actually moved
+
     if moved:       #if moved then copy new board into original one
         for r in range(4):
             board[r] = new[r][:] 
@@ -98,9 +100,8 @@ def check_game_over(board):
         return False
     for r in range(4):
         for c in range(4):
-            if r < 3 and board[r][c] == board[r+1][c]:
+            if r<3 and board[r][c] == board[r+1][c]:
                 return False
-            if c < 3 and board[r][c] == board[r][c+1]:
+            if c<3 and board[r][c] == board[r][c+1]:
                 return False
     return True
-
